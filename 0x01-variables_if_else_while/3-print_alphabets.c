83c1b1e6c16687c321b7f9fbee0f4c1ebe38f220
eliyan27 <eliyanmoel@gmail.com>
2023-02-26 05:33:10 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+imt main(void)
+{
+	char laow;
+
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+	for (low = 'A'; low <= 'Z'; low++)
+	putchar(low);
+	putchar('\n');
+	return (0);
+}
