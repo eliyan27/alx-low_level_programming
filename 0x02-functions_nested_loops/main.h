313664fc2b41cea22f35fc667843980c9fb89497
eliyan27 <eliyanmoel@gmail.com>
2023-02-26 04:25:54 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+int _putchar(char c);
+void print_alphabet(void);
+void print-alphabet_x10(void);
+int _islower(int c);
+int _alpha(int c);
+int print_sign(int a);
+int _abs(int);
+int print_last-digit(int);
+void jack_bauer(void);
+void times_tables(void);
+int add(int,int);
+void print_to_98(int a);
+void print_times_table(int a);
